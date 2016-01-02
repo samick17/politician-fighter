0d69b4048c1af6190df87169b1dce60f88c17484
samick17 <boneache@gmail.com>
2016-01-03 05:16:30 +0800

    add base skill generator

--- public/js/arena.js
+++ public/js/arena.js
@@ -3,131 +3,145 @@ var Arena = function(game) {};
 (function() {
 
   const BaseLineHeight = 32;
+  const VMotionState = {UP: 0, DOWN: 1, NONE: 2};
+  const HMotionState = {LEFT: 0, RIGHT: 1, NONE: 2};
 
-  var character1;
-  
+  var playerObj;
+  var fireballs;
+  var nextJump = Number.NEGATIVE_INFINITY, nextMoveDown = Number.NEGATIVE_INFINITY, nextInputThreshold = 20, lastInputTime = Number.NEGATIVE_INFINITY, inputTimeout = Number.NEGATIVE_INFINITY, nextFire = Number.NEGATIVE_INFINITY, fireRate = 300;
 
-  function createCharacter(resName) {
-    var characterGroup = game.add.group();
-    var characterSpr = game.add.sprite(0, 0, resName);
-    const scale = 2;
-    const offsetXScale = 0.2*scale;
-    const widthScale = 0.6*scale;
-    characterGroup.scale.setTo(scale, scale);
-    characterGroup.add(characterSpr);
-    game.physics.enable(characterSpr, Phaser.Physics.ARCADE);
-    characterSpr.pivot.x = characterSpr.width/2;
-    characterSpr.pivot.y = characterSpr.height/2;
-    characterSpr.body.setSize(characterSpr.width*widthScale, characterSpr.height*scale, characterSpr.width*offsetXScale, 0);
-    function Character(spr) {
-      this.life = 50;
-      this.maxLife = 50;
-      this.spr = spr;
-      this.walkVelocity = 5;
-      this.velocity = this.walkVelocity;
-      this.runVelocity = 9;
-      this.baseLine = 0;
-    };
-    Character.prototype.setPos = function(x, y) {
-      this.spr.x = x;
-      this.spr.y = y;
-      return this;
-    };
-    Character.prototype.getLifeRate = function() {
-      return this.life/this.maxLife;
-    };
-    Character.prototype.applyDamage = function(dmg) {
-      this.life = MathUtils.clamp(this.life+dmg, 0, this.maxLife);
-      return this;
-    };
-    Character.prototype.getSprBody = function() {
-      return characterSpr;
-    };
-    Character.prototype.turnLeft = function() {
-      characterSpr.scale.setTo(-1, 1);
-      characterSpr.body.setSize(characterSpr.width*widthScale, characterSpr.height*scale, characterSpr.width*offsetXScale, 0);
-    };
-    Character.prototype.turnRight = function() {
-      characterSpr.scale.setTo(1, 1);
-      characterSpr.body.setSize(characterSpr.width*widthScale, characterSpr.height*scale, characterSpr.width*offsetXScale, 0);
-    };
-    Character.prototype.moveLeft = function() {
-      this.turnLeft();
-      this.spr.x -= this.velocity;
-    };
-    Character.prototype.moveRight = function() {
-      this.turnRight();
-      this.spr.x += this.velocity;
-    };
-    Character.prototype.jump = function() {
-      if(this.isJump)
-        return;
-      this.turnRight();
-      characterSpr.body.velocity.setTo(characterSpr.body.velocity.x, -180);
-      this.isJump = true;
-    };
-    Character.prototype.update = function() {
-      if(this.isJump) {
-        //ground
-        if(characterSpr.y > this.baseLine*BaseLineHeight) {
-          this.grounded();
-        }
-      }
-      else {
-        if(characterSpr.y > this.baseLine*BaseLineHeight) {
-          characterSpr.y = this.baseLine*BaseLineHeight
-        }
-      }
+  var skillGenerator = {
+    'lrr': function() {
+      fire_now();
     },
-    Character.prototype.grounded = function() {
-      if(!this.isJump)
-        return;
-      this.applyGravity(0);
-      characterSpr.body.velocity.setTo(characterSpr.body.velocity.x, 0);
-      characterSpr.y = this.baseLine*BaseLineHeight;
-      this.isJump = false;
+    'dla': function() {
+      fire_now();
     },
-    Character.prototype.runMode = function() {
-      this.velocity = this.runVelocity;
-    };
-    Character.prototype.walkMode = function() {
-      this.velocity = this.walkVelocity;
-    };//getBody
-    Character.prototype.applyGravity = function(gravity) {
-      characterSpr.body.gravity.set(0, gravity);
-    };
-    return new Character(characterGroup);
+    'dra': function() {
+      fire_now();
+    },
+    'dja': function() {
+      fire_now();
+      nextFire = game.time.now-5;
+      fire_now();
+    }
+  };
+
+  var currentInput = [];
+  function appendInput(inputText) {
+    if(game.time.now - lastInputTime > nextInputThreshold) {
+      currentInput.push(inputText);
+      var currentInputValue = currentInput.join('');
+      if(skillGenerator[currentInputValue]) {
+        skillGenerator[currentInputValue]();
+        currentInput = [];
+        inputTimeout = Number.NEGATIVE_INFINITY;
+      }
+      inputTimeout = game.time.now + 300;
+      lastInputTime = game.time.now;
+    }
   }
 
-  function createLifeBar() {
-    var lifeBarGroup = game.add.group();
-    var lifeBar = game.add.image(0, 0, 'life-bar');
-    var lifeBarContent = game.add.image(6, 2, 'life-bar-content');
-    lifeBarGroup.scale.setTo(0.65,0.65);
-    lifeBarGroup.add(lifeBar);
-    lifeBarGroup.add(lifeBarContent);
-    var lifeBarWidth = lifeBarContent.width;
-    return {
-      getSpr: function() {
-        return  lifeBarGroup;
-      },
-      setPos: function(x, y) {
-        lifeBarGroup.x = x;
-        lifeBarGroup.y = y;
-        return this;
-      },
-      setRate: function(rate) {
-        lifeBarContent.width = lifeBarWidth*rate;
-        return this;
-      },
-      getHeight: function() {
-        return lifeBarGroup.height
-      },
-      update: function() {
-        this.setRate(this.target.getLifeRate());
-        //this.setPos(-this.target.spr.width/2, -this.target.spr.height/2-this.getHeight());
+  function createPlayer(x, baseLine) {
+    var player = game.add.sprite(x, baseLine*BaseLineHeight, 'firzen');
+    player.pivot.x = player.width/2;
+    player.pivot.y = player.height/2;
+    game.physics.enable(player, Phaser.Physics.ARCADE);
+    player.body.gravity.set(0, 0);
+    player.scale.setTo(2,2);
+    function Player(spr) {
+      this.spr = spr;
+      this.isJump = false;
+      this.baseLine = baseLine;
+      this.vMotionState = VMotionState.NONE;
+      this.hMotionState = HMotionState.NONE;
+    }
+    Player.prototype.turnLeft = function() {
+      this.spr.scale.setTo(-2, 2);
+      this.spr.body.setSize(-player.width/2, player.height/2, player.width, 0);
+    };
+    Player.prototype.turnRight = function() {
+      this.spr.scale.setTo(2, 2);
+      this.spr.body.setSize(-player.width/2, player.height/2, player.width, 0);
+    };
+    Player.prototype.jump = function() {
+      if (!this.isJump && game.time.now > nextJump){
+        this.spr.body.velocity.setTo(this.spr.body.velocity.x, -400);
+        player.body.gravity.set(0, 900);
+        nextJump = game.time.now + 900;
+        this.isJump = true;
       }
     };
+    Player.prototype.moveUp = function() {
+      if (!this.isJump && game.time.now > nextMoveDown){
+        this.baseLine -= 1;
+        game.add.tween(this.spr.body).to({ y: this.baseLine * BaseLineHeight }, 100, Phaser.Easing.Linear.None, true, null, 0, false);
+        nextMoveDown = game.time.now + 100;
+      }
+    };
+    Player.prototype.moveDown = function() {
+      if (!this.isJump && game.time.now > nextMoveDown){
+        this.baseLine += 1;
+        game.add.tween(this.spr.body).to({ y: this.baseLine * BaseLineHeight }, 100, Phaser.Easing.Linear.None, true, null, 0, false);
+        nextMoveDown = game.time.now + 100;
+      }
+    };
+    Player.prototype.moveLeft = function() {
+      this.turnLeft();
+      this.spr.body.x -= 5;
+    };
+    Player.prototype.moveRight = function() {
+      this.turnRight();
+      this.spr.body.x += 5;
+    };
+    Player.prototype.grounded = function() {
+      this.spr.body.y = this.baseLine * BaseLineHeight;
+      this.spr.body.gravity.set(0, 0);
+      this.spr.body.velocity.setTo(this.spr.body.velocity.x, 0);
+      this.isJump = false;
+    }
+    Player.prototype.update = function() {
+      switch(this.vMotionState) {
+        case VMotionState.UP:
+        this.moveUp();
+        break;
+        case VMotionState.DOWN:
+        this.moveDown();
+        break;
+      }
+      switch(this.hMotionState) {
+        case HMotionState.LEFT:
+        this.moveLeft();
+        break;
+        case HMotionState.RIGHT:
+        this.moveRight();
+        break;
+      }
+      if(this.isJump && this.spr.body.y > this.baseLine * BaseLineHeight) {
+        this.grounded();
+      }
+    };
+    return new Player(player);
+  }
+
+  function fire_now() {
+    if (game.time.now > nextFire){
+        nextFire = game.time.now + fireRate;
+        var fireball = fireballs.getFirstExists(false); // get the first created fireball that no exists atm
+        if (fireball) {
+            fireball.exists = true;  // come to existance !
+            fireball.lifespan=2500;  // remove the fireball after 2500 milliseconds - back to non-existance
+            if(playerObj.spr.scale.x < 0){  // if player looks to the left - create the fireball on his left side
+              fireball.reset(playerObj.spr.x-20, playerObj.spr.y);
+                game.physics.enable(fireball, Phaser.Physics.ARCADE);
+                fireball.body.velocity.setTo(-800,0);
+            }else{
+                fireball.reset(playerObj.spr.x+20, playerObj.spr.y);
+                game.physics.enable(fireball, Phaser.Physics.ARCADE);
+                fireball.body.velocity.setTo(800,0);
+            }
+        }
+    }
   }
 
   Arena.prototype = {
@@ -137,32 +151,51 @@ var Arena = function(game) {};
       game.load.image('life-bar', 'media/arena/life-bar.png');
       game.load.image('life-bar-content', 'media/arena/life-bar-content.png');
       game.load.spritesheet('firzen', 'media/arena/firzen_1.bmp', 80, 80, 70);
+      game.load.image('fireball', 'media/arena/fireball.png',40,30);
     },
     create: function() {
+      const BackgroundScale = 5;
+      game.world.setBounds(0, 0, BackgroundScale*game.width, game.height);
       game.physics.startSystem(Phaser.Physics.ARCADE);
-      bg1 = game.add.tileSprite(0, 0, 5333, 1396, 'bg');
-      bg1.scale.setTo(2, 0.2);
-      bg1.tileScale.x = 0.2;
-      bg2 = game.add.tileSprite(0, 0, 5333, 1588, 'bg2');
-      bg2.scale.setTo(2, 0.2);
-      bg2.y = 279;
-      bg2.tileScale.x = 0.2;
-      character1 = createCharacter('firzen').setPos(100, game.height-500);
-      game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(()=>{
-        character1.jump();
-        character1.applyGravity(480);
-      }, this, 0);
+      bg1 = game.add.tileSprite(0, 0, game.width, 128, 'bg');
+      bg1.scale.setTo(BackgroundScale, 1);
+      bg1.tileScale.x = 1/BackgroundScale;
+      bg2 = game.add.tileSprite(0, 128, game.width, 896, 'bg2');
+      bg2.scale.setTo(BackgroundScale, 1);
+      bg2.tileScale.x = 1/BackgroundScale;
+
+      fireballs = game.add.group();  // add a new group for fireballs
+      fireballs.createMultiple(500, 'fireball', 0, false);
+
+      playerObj = createPlayer(500,5);
+
+      cursors = game.input.keyboard.createCursorKeys();
+      game.camera.follow(playerObj.spr);
+
+      var keyUp = game.input.keyboard.addKey(Phaser.Keyboard.NUMPAD_8);
+      keyUp.onDown.add(() => {playerObj.vMotionState = VMotionState.UP;playerObj.moveUp();appendInput('u');}, this)
+      keyUp.onUp.add(() => {playerObj.vMotionState = VMotionState.NONE}, this);
+      var keyDown = game.input.keyboard.addKey(Phaser.Keyboard.NUMPAD_2)
+      keyDown.onDown.add(() => {playerObj.vMotionState = VMotionState.DOWN;playerObj.moveDown();appendInput('b');}, this);
+      keyDown.onUp.add(() => {playerObj.vMotionState = VMotionState.NONE}, this);
+      var keyLeft = game.input.keyboard.addKey(Phaser.Keyboard.NUMPAD_4)
+      keyLeft.onDown.add(() => {playerObj.hMotionState = HMotionState.LEFT;playerObj.moveLeft();appendInput('l');}, this);
+      keyLeft.onUp.add(() => {playerObj.hMotionState = HMotionState.NONE}, this);
+      var keyRight = game.input.keyboard.addKey(Phaser.Keyboard.NUMPAD_6)
+      keyRight.onDown.add(() => {playerObj.hMotionState = HMotionState.RIGHT;playerObj.moveRight();appendInput('r');}, this);
+      keyRight.onUp.add(() => {playerObj.hMotionState = HMotionState.NONE}, this);
+      game.input.keyboard.addKey(Phaser.Keyboard.NUMPAD_5)
+      .onDown.add(() => {console.log('atk');appendInput('a');}, this);
+      game.input.keyboard.addKey(Phaser.Keyboard.NUMPAD_0)
+      .onDown.add(() => {playerObj.jump();appendInput('j');}, this);
+      game.input.keyboard.addKey(Phaser.Keyboard.NUMPAD_ADD)
+      .onDown.add(() => {console.log('def');appendInput('d');}, this);
     },
     update: function() {
-      if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
-      {
-        character1.moveLeft();
+      playerObj.update();
+      if(game.time.now > inputTimeout) {
+        currentInput = [];
       }
-      else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
-      {
-        character1.moveRight();
-      }
-      character1.update();
     },
     render: function() {
     }
